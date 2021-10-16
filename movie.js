936b35e6cae1898203ac3a6a4c19f6a85e092a40
Matthias Hinojosa <hinojosamatthias@gmail.com>
2021-10-16 17:59:20 -0500

    another save

--- movie.js
+++ movie.js
@@ -13,4 +13,18 @@ movies.then(function (result) {
 })
 })
 
+fetch('https://almondine-abiding-title.glitch.me/movies').then(response =>{
+    response.json().then(titles =>{
+        titles.forEach( titleObj =>{
+            console.log(titleObj.title);
+            var titleMovie = titleObj.title.join(' ,')
+            $('#demo').append(titleMovie)
+        })
+    })
+})
 
+//hello
+// var nameString = users.map(function (user){
+//     return user.name
+// }).join(', ')
+// console.log(nameString);
